9bb0b03dad63e86b84db380e3ee5872191dda718
Hays <hayblackwell@gmail.com>
2018-09-28 13:40:35 -0500

    Add project files.

---
+++ Best By/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Best_By
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
